2c53afb525530ed632b1d67c531d0fbb220e06cc
Michael Hucka <mhucka@caltech.edu>
2014-05-07 12:23:18 -0400

    Path didn't include the necesary bin directory after a reboot.

--- src/server/run.sh
+++ src/server/run.sh
@@ -2,6 +2,9 @@
 if [ `id -u` = 0 ]; then
     echo "Do not run this as root; run it as mhucka"
 else
+    PATH=$PATH:/home/mhucka/.cabal/bin
+    export PATH
+
     . ./venv/bin/activate
     ./flask-server.py
 fi
